7b8c5ef4aaf1f5c62039cc3ab12bbd7d28c2e197
Pratik <tik.shah7@gmail.com>
2017-04-01 18:00:09 +0530

    added logger to the resource class

--- src/main/java/com/operations/resources/StringOperationResource.java
+++ src/main/java/com/operations/resources/StringOperationResource.java
@@ -1,6 +1,8 @@
 package com.operations.resources;
 
 import com.codahale.metrics.annotation.Timed;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -9,6 +11,8 @@ import javax.ws.rs.core.Response;
 @Path("/")
 @Produces(MediaType.TEXT_PLAIN)
 public class StringOperationResource {
+
+    private static final Log logger = LogFactory.getLog(StringOperationResource.class);
 
     public StringOperationResource() {
     }
@@ -26,13 +30,16 @@ public class StringOperationResource {
     public String stringReversal(@QueryParam("str") String str) {
 
         if(str==null ){
+            logger.error("Null String provided.");
             return Response.Status.NOT_ACCEPTABLE.toString();
         }
         else if( str.equals("")){
+            logger.info("Blank String provided.");
             return str;
         }
 
         String reversedString= new StringBuilder(str).reverse().toString();
+        logger.info("Reversed string : "+ reversedString +" for the orginal string : " +str);
 
         return reversedString;
     }
@@ -43,19 +50,25 @@ public class StringOperationResource {
     public String wordsReversalInASentence(@QueryParam("str") String str) {
 
         if(str==null ){
+            logger.error("Null String provided.");
             return Response.Status.NOT_ACCEPTABLE.toString();
         }
         else if( str.equals("")){
+            logger.info("Blank String provided.");
            return str;
         }
 
         String sentence= new StringBuilder(str).toString();
         String[] words = sentence.split(" ");
         StringBuilder stringBuilder =new StringBuilder();
+        String reversedString;
         for (String word:words) {
             stringBuilder.append(stringReversal(word)).append(' ');
         }
         stringBuilder.setLength(stringBuilder.length() - 1);
-        return stringBuilder.toString();
+        reversedString= stringBuilder.toString();
+        logger.info("Reversed string of words: "+ reversedString +" for the orginal sentence : " +str);
+
+        return reversedString;
     }
 }
